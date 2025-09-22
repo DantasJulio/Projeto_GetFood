623abbd26eda139290187f4e876aa08003190f1d
Julio Dantas <juliodantas19@gmail.com>
2025-09-22 17:38:32 -0300

    Feature - Configurando o serviço de pagamentos para se auto-registrar no Eureka.

--- pagamentos/src/main/java/br/com/getfood/pagamentos/PagamentosApplication.java
+++ pagamentos/src/main/java/br/com/getfood/pagamentos/PagamentosApplication.java
@@ -1,5 +1,6 @@
 package br.com.getfood.pagamentos;
 
+import com.netflix.discovery.EurekaNamespace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
