8a8604f8fae1e02cdd36b471026452c05d0ac66e
Julio Dantas <juliodantas19@gmail.com>
2025-09-24 15:08:11 -0300

    Criando o gateway e o serviço de pedidos baseado no de pagamentos.

---
+++ pedidos/src/main/java/br/com/getfood/pedidos/dto/ItemDoPedidoDto.java
@@ -0,0 +1,14 @@
+package br.com.getfood.pedidos.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ItemDoPedidoDto {
+
+    private Long id;
+    private Integer quantidade;
+    private String descricao;
+}
